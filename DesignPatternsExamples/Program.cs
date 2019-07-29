2216da30ef3e86d65928c0c854b9029354af085b
Kerollos Adel <kerollos.sobhi@axa-egypt.com>
2019-07-29 19:38:14 +0200

    add Singleton

--- DesignPatternsExamples/Program.cs
+++ DesignPatternsExamples/Program.cs
@@ -4,9 +4,48 @@ namespace DesignPatternsExamples
 {
     class Program
     {
+        /// <summary> 
+        /// Entry point into console application. 
+        /// </summary>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Constructor is protected -- cannot use new 
+            Singleton s1 = Singleton.Instance();
+            Singleton s2 = Singleton.Instance();
+
+            // Test for same instance 
+            if (s1 == s2)
+            {
+                Console.WriteLine("Objects are the same instance");
+            }
+
+            // Wait for user   
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// The 'Singleton' class
+        /// </summary>
+        class Singleton
+        {
+            private static Singleton _instance;
+
+            // Constructor is 'protected'
+            protected Singleton()
+            {
+            }
+
+            public static Singleton Instance()
+            {
+                // Uses lazy initialization.
+                // Note: this is not thread safe.
+                if (_instance == null)
+                {
+                    _instance = new Singleton();
+                }
+
+                return _instance;
+            }
         }
     }
 }
